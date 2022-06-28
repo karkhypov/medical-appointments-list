0de1ffb711b47cfcfed25b5151478f8ee599189a
karkhypov <kirill.arkhypov.work@gmail.com>
2022-06-28 16:43:38 +0300

    implemented data rendering, filtering by date and appointmen time highlight

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
+import { useEffect, useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+import SelectVariants from './components/SelectVariants';
+import AppointmentCard from './components/AppointmentCard';
+
+import { getDate, getDuration, getTime, sortByDate, groupBy } from './utils';
+
+import Data from './data.json';
+
 const App = () => {
-  return <div>Hello!</div>;
+  const [initialData, setInitialData] = useState(Data);
+
+  const groupByClinicianName = groupBy(['clinicianName']);
+  const groupByStartDate = groupBy(['startDate']);
+  console.log(groupByClinicianName(Data));
+  console.log(groupByStartDate(Data));
+
+  return (
+    <Container maxWidth='lg' sx={{ mt: '25px', mb: '25px' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          width: '100%',
+          mb: '25px',
+        }}
+      >
+        <SelectVariants />
+        <Button variant='contained'>New Appointment</Button>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+        }}
+      >
+        {sortByDate(initialData).map(
+          ({ id, patient: { name }, clinicianName, startDate, endDate }) => {
+            return (
+              <AppointmentCard
+                key={id}
+                patientName={name}
+                clinicianName={clinicianName}
+                startDate={getDate(startDate)}
+                startTime={getTime(startDate)}
+                duration={getDuration(startDate, endDate)}
+              />
+            );
+          }
+        )}
+      </Box>
+    </Container>
+  );
 };
 
 export default App;
